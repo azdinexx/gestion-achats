8d7b4aebd78e6bfd4dcda77df8f57d9e6d4b8787
azdinexx <azdinebouali2002@gmail.com>
2024-04-27 20:53:21 +0100

    Update NotificationSection component styles

--- src/components/NotificationSection.tsx
+++ src/components/NotificationSection.tsx
@@ -13,8 +13,12 @@ function NotificationSection() {
         <Bell width={24} height={24} />
       </button>
       <div
-        className='w-1/3 border-l-2 border-stone-300 h-full flex flex-col p-4'
-        style={{ display: mini ? 'none' : 'block' }}
+        className='w-1/3 border-l-2 border-stone-300 h-full flex flex-col p-4 transition-all duration-300 ease-in-out bg-white shadow-lg'
+        style={{
+          width: mini ? '0' : '30%',
+          height: mini ? '0' : '100%',
+          opacity: mini ? 0 : 1,
+        }}
       >
         <div className=' flex pr-4  items-end'>
           <h1 className='font-bold text-2xl'>Notifications</h1>
